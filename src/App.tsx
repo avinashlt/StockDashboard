a010d402b1d50c9c48d7a6f22be58c7a5deeb2a7
shubham9671 <sj120951@gmail.com>
2024-10-14 14:58:58 +0530

    header implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import StocksMarketDashboard from './stocksDashboardWrapper/StocksMarketDashboard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <StocksMarketDashboard/>
     </div>
   );
 }
---
+++ src/stocksDashboardWrapper/StocksMarketDashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../common/header';
+
+function StocksMarketDashboard() {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  )
+}
+
+export default StocksMarketDashboard
